48ea2002b05af30ce745e188538ca65fff18ff31
Blair Guardia <blairguardia@gmail.com>
2021-05-19 21:46:12 +0900

    Installed application gems

---
+++ app/views/twoots/_twoot.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! twoot, :id, :body, :created_at, :updated_at
+json.url twoot_url(twoot, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :twoots
+  root "twoots#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
