8b98237dacce8ecd73cda6f6632a485ec76b46c7
Blair Guardia <blairguardia@gmail.com>
2021-05-20 13:06:58 +0900

    Update login form templates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_031852) do
+ActiveRecord::Schema.define(version: 2021_05_20_033140) do
 
   create_table "twoots", force: :cascade do |t|
     t.text "body"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(version: 2021_05_20_031852) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
