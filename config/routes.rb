d57192ef41df2e87fb787be9a706d8c0c90ef539
Blair Guardia <blairguardia@gmail.com>
2021-05-22 21:04:25 +0900

    Update routes for replies, follows and likes

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,15 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
   # likes
-  get "like/twoot/:twoot_id", to: "likes#create"
-  delete "unlike/twoot/:twoot_id", to: "likes#destroy"
+  get "like/twoot/:twoot_id", to: "likes#create", as: :like
+  delete "unlike/twoot/:twoot_id", to: "likes#destroy", as: :unlike
 
   # follows
-  get "follow/user/:followed_id", to: "follows#create"
-  delete "unfollow/user/:followed_id", to: "follows#destroy"
+  get "follow/user/:followed_id", to: "follows#create", as: :follow
+  delete "unfollow/user/:followed_id", to: "follows#destroy", as: :unfollow
 
   #replies
-  get "reply/twoot/:reply_id", to: "replies#new"
-  post "reply", to: "replies#create"
+  resources :replies, only: [:new, :create]
 
   # resources :likes, only: [:create, :destroy]
   root "twoots#index"
