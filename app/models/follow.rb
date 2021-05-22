3e5bfab4938fe4154c9ee1df559431f3a2e73012
Blair Guardia <blairguardia@gmail.com>
2021-05-22 21:09:58 +0900

    Update SQL query for follow suggestions

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,6 +5,6 @@ class Follow < ApplicationRecord
       User.where("users.id NOT IN 
                   (SELECT follows.followed_id 
                   FROM follows 
-                  WHERE follows.follower_id = '#{user_id}') AND users.id != '#{user_id}'").limit(lim)
+                  WHERE follows.follower_id = :user_id) AND users.id != :user_id", user_id: user_id).limit(lim)
     end
 end
